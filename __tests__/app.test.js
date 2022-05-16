26e39b9551193692fafd82020ddfc054fdbd076b
EduardoAquinta <edwoodashton@yahoo.co.uk>
2022-05-16 13:03:29 +0100

    Still stuck on the modules not being imported for app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,3 +1,5 @@
+const seed = require("../db/seeds");
+const testData = require("../db/data/test-data")
 const request = require('supertest');
 const app = require('../app');
 const db = require('../db');
@@ -5,6 +7,10 @@ const db = require('../db');
 // afterAll(() => {
 //     if (db.end) db.end();
 // })
+
+beforeEach(() => {
+return seed(testData)
+});
 
 describe ("GET: /api", () => {
     test("status 200: returns a message as an JSON object", () => {
