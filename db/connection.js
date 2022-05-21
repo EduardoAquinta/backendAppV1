1b94f33202688c0764d03c1d1a8edaf92a9a6038
EduardoAquinta <edwoodashton@yahoo.co.uk>
2022-05-21 20:07:47 +0100

    next attempt3

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
 
-const config =
+const config = 
   ENV === 'production'
     ? {
         connectionString: process.env.DATABASE_URL,
@@ -11,8 +11,9 @@ const config =
         },
       }
     : {};
+    ;
 
-    
+
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
